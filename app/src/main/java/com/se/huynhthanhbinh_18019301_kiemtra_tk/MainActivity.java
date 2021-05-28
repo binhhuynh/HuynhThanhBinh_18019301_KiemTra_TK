dba3ec7531ca5d1cd56daecedde8ec0045539c27
Thanh Bình Huỳnh <huynhthanhbinh290@gmail.com>
2021-05-28 14:21:52 +0700

    commit cau 1

--- app/src/main/java/com/se/huynhthanhbinh_18019301_kiemtra_tk/MainActivity.java
+++ app/src/main/java/com/se/huynhthanhbinh_18019301_kiemtra_tk/MainActivity.java
@@ -1,14 +1,57 @@
 package com.se.huynhthanhbinh_18019301_kiemtra_tk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText etEmail, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogin();
+            }
+        });
+    }
+
+    private void userLogin() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(MainActivity.this, TabletActivity.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/se/huynhthanhbinh_18019301_kiemtra_tk/TabletActivity.java
@@ -0,0 +1,14 @@
+package com.se.huynhthanhbinh_18019301_kiemtra_tk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TabletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tablet);
+    }
+}
